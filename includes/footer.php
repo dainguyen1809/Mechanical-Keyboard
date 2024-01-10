e9f995df01ccd20db7f3c7cdab7bd7ce6270b3c5
dainguyen.dhn <dainguyen.nhd@gmail.com>
2024-01-10 20:16:47 +0700

    update ajax

--- includes/footer.php
+++ includes/footer.php
@@ -3,13 +3,13 @@
     <div class="container py-5">
         <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5) ;">
             <div class="row g-4">
-                <div class="col-lg-3">
+                <div class="col-9">
                     <a href="#">
                         <h1 class="text-primary mb-0">Keyzone</h1>
                         <p class="text-secondary mb-0">High quality products</p>
                     </a>
                 </div>
-                <div class="col-lg-6">
+                <!-- <div class="col-lg-6">
                     <div class="position-relative mx-auto">
                         <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="number"
                             placeholder="Your Email">
@@ -17,7 +17,7 @@
                             class="btn btn-primary border-0 border-secondary py-3 px-4 position-absolute rounded-pill text-white"
                             style="top: 0; right: 0;">Subscribe Now</button>
                     </div>
-                </div>
+                </div> -->
                 <div class="col-lg-3">
                     <div class="d-flex justify-content-end pt-3">
                         <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle"
@@ -100,10 +100,12 @@
 <!-- JavaScript Libraries -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 <script src="./assets/lib/easing/easing.min.js"></script>
 <script src="./assets/lib/waypoints/waypoints.min.js"></script>
 <script src="./assets/lib/lightbox/js/lightbox.min.js"></script>
 <script src="./assets/lib/owlcarousel/owl.carousel.min.js"></script>
+
 
 <!-- Template Javascript -->
 <!-- <script src="./assets/js/main.js"></script> -->
